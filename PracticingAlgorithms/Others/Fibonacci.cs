174257bb268c901cbff82107ed301b34d9241965
Leonam Santos <leonam.santos@deliveryhero.com>
2018-12-11 17:06:38 +0100

    Add fibonacci exercise

---
+++ PracticingAlgorithms/Others/Fibonacci.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticingAlgorithms.Others
+{
+    public class Fibonacci 
+    {
+        private static IEnumerable<int> ElementsWithYield()
+        {
+            int current = 1, previous = 0;
+            while(true)
+            {
+                yield return current;
+                int old = current;
+                current += previous;
+                previous = old;
+            }
+        }
+
+        public static int[] Elements(int size)
+        {
+            var result = new List<int>();
+            foreach (var n in ElementsWithYield())
+            {
+                result.Add(n);
+                if (--size <= 0)
+                    break;
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ PracticingAlgorithms/Others/FibonacciTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticingAlgorithms.Others
+{
+    [TestFixture]
+    public class FibonacciTest
+    {
+        [Test]
+        public void ShouldReturnTheFirstFibonacciNumber()
+        {
+            var actual = Fibonacci.Elements(1);
+
+            Assert.That(actual.Length, Is.EqualTo(1));
+            Assert.That(actual, Is.EqualTo(new int[] { 1 }));
+        }
+
+        [Test]
+        public void ShouldReturnTheFirstThreeFibonacciNumbers()
+        {
+            var actual = Fibonacci.Elements(3);
+
+            Assert.That(actual.Length, Is.EqualTo(3));
+            Assert.That(actual, Is.EqualTo(new int[] { 1, 1, 2 }));
+        }
+
+        [Test]
+        public void ShouldReturnTheFirstTenFibonacciNumbers()
+        {
+            var actual = Fibonacci.Elements(10);
+
+            Assert.That(actual.Length, Is.EqualTo(10));
+            Assert.That(actual, Is.EqualTo(new int[] { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 }));
+        }
+    }
+}
